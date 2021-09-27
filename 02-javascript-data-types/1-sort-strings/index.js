07989ffe956c6af678f8e3cb9648e3436d63152f
mkhv0419 <mariia.khvostova@netcracker.com>
2021-09-27 22:22:57 +0400

    Lesson 2, task 1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,13 +6,10 @@
  */
 export function sortStrings(arr, param = 'asc') {
   const sortedArray = [...arr];
-  if (param === 'asc') {
-    return sortedArray.sort((a, b) => {
-      return a.localeCompare(b, ['ru', 'en'], {sensitivity: "variant", caseFirst: "upper"});
-    });
-  } else {
-    return sortedArray.sort((a, b) => {
-      return b.localeCompare(a, ['ru', 'en'], {sensitivity: "variant", caseFirst: "upper"});
-    });
-  }
+  return sortedArray.sort((a, b) => {
+    const locales = ['ru', 'en'];
+    const options = {sensitivity: "variant", caseFirst: "upper"};
+    return param === 'asc' ? a.localeCompare(b, locales, options) :
+      b.localeCompare(a, locales, options);
+  });
 }

3806b0dd6287655d233353f16e0b40e3692f8dec
mkhv0419 <mariia.khvostova@netcracker.com>
2021-10-01 18:25:42 +0400

    lesson 3 task 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,20 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let previousChar = '';
+  let similarCharCount = 0;
+  return size === 0 ? '' : string.split('').filter(function (char) {
+    if (char === previousChar) {
+      similarCharCount++;
+      if (similarCharCount > size) {
+        similarCharCount--;
+        return false;
+      }
+      return true;
+    } else {
+      previousChar = char;
+      similarCharCount = 1;
+      return true;
+    }
+  }).join('');
 }

8c8fef992599edae177c3cf5f8d732cff24c66c4
mkhv0419 <mariia.khvostova@netcracker.com>
2021-10-12 11:05:28 +0400

    lesson 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  element;
+  static activeNotificationElement;
+  static timer;
 
+  constructor(notificationMessage = '', {duration = 0, type = ''} = {}) {
+    this.notificationMessage = notificationMessage;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+     <div class="notification ${this.type}" style="--value:${this.duration}ms">
+       <div class="timer"></div>
+       <div class="inner-wrapper">
+         <div class="notification-header">${this.type}</div>
+         <div class="notification-body">
+           ${this.notificationMessage}
+         </div>
+       </div>
+     </div>`;
+  }
+
+  show(targetElement) {
+    if (NotificationMessage.activeNotificationElement) {
+      clearTimeout(NotificationMessage.timer);
+      this.remove();
+    }
+
+    this.render();
+    NotificationMessage.activeNotificationElement = this.element;
+    if (targetElement) {
+      targetElement.appendChild(this.element);
+    } else {
+      document.body.append(this.element);
+    }
+    NotificationMessage.timer = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  remove() {
+    this.element.remove();
+    NotificationMessage.activeNotificationElement.remove();
+    NotificationMessage.activeNotificationElement = null;
+  }
+
+  destroy() {
+    this.element.remove();
+    NotificationMessage.activeNotificationElement = null;
+    NotificationMessage.timer = null;
+  }
 }
